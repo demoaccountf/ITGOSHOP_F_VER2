9e0427ab5e2eb9562a4838d5bda440a61a59473a
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-11-28 08:00:17 +0700

    COMMIT

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace ITGoShop_F_Ver2.Controllers
             ViewBag.AllSubBrand = context.getAllSubBrand();
             ViewBag.AllBlog = context.getAllBlog();
             ViewBag.AllBannerSlider = context.getAllBannerSlider();
+            ViewBag.Top3Product = context.getTop3ProductView();
             return View();
         }
 
