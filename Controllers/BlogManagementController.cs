d3897ee8364ba65ac9f7b2d366c75552ddfb5a44
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-12-01 18:09:53 +0700

    commit

---
+++ Controllers/BlogManagementController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITGoShop_F_Ver2.Controllers
+{
+    public class BlogManagementController : Controller
+    {
+        public IActionResult add_content()
+        {
+            return View();
+        }
+        public IActionResult view_content()
+        {
+            return View();
+        }
+    }
+}
