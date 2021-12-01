2ae79be75134ac27efdbdf70014a1a92ba99abfc
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-12-01 13:11:30 +0700

    commit

---
+++ Controllers/ProductCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITGoShop_F_Ver2.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        public IActionResult add_product_category()
+        {
+            return View();
+        }
+        public IActionResult all_product_category()
+        {
+            return View();
+        }
+    }
+}
