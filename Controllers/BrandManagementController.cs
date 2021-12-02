fff4226175015aa1a122f4868e871ab676dc2a02
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-12-02 18:22:17 +0700

    commit

--- Controllers/BrandManagementController.cs
+++ Controllers/BrandManagementController.cs
@@ -20,5 +20,21 @@ namespace ITGoShop_F_Ver2.Controllers
         {
             return View();
         }
+        public void unactive_category(string CategoryId)
+        {
+            ITGoShopContext context = HttpContext.RequestServices.GetService(typeof(ITGoShop_F_Ver2.Models.ITGoShopContext)) as ITGoShopContext;
+            context.updateCateStatus(CategoryId, 0);
+        }
+        public void active_category(string CategoryId)
+        {
+            ITGoShopContext context = HttpContext.RequestServices.GetService(typeof(ITGoShop_F_Ver2.Models.ITGoShopContext)) as ITGoShopContext;
+            context.updateCateStatus(CategoryId, 1);
+        }
+
+        public void delete_category(string CategoryId)
+        {
+            var context = new ITGoShopLINQContext();
+            context.deleteCategory(CategoryId);
+        }
     }
 }
